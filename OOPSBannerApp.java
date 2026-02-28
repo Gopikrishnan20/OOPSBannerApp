758c66be7bb7eaec94c95dfac40a496bc056b20e
Gopikrishnan20 <algopikrishnan20@gmail.com>
2026-02-28 17:40:00 +0530

    first

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,28 +1,32 @@
-/**
- * OOPSBannerApp
- *
- * <p>UC2: Displays the word "OOPS" as a large ASCII banner
- * using individual print statements.</p>
- *
- * @author Gopi Krishnan
- * @version 2.0
- */
-public class OOPSBannerApp {
+public class OOPSBannerApp  {
 
-    /**
-     * Entry point of the application.
-     *
-     * @param args Command-line arguments
-     */
     public static void main(String[] args) {
 
-        System.out.println(" *****     *****    *****    ****** ");
-        System.out.println("*     *   *     *   *     *  *     *");
-        System.out.println("*     *   *     *   *     *  *     *");
-        System.out.println("*     *   *     *   ******   ****** ");
-        System.out.println("*     *   *     *   *        *      ");
-        System.out.println("*     *   *     *   *        *      ");
-        System.out.println(" *****     *****    *        *      ");
+        String O1 = " ***** ";
+        String O2 = "*     *";
+        String O3 = "*     *";
+        String O4 = "*     *";
+        String O5 = " ***** ";
 
+        String P1 = "****** ";
+        String P2 = "*     *";
+        String P3 = "****** ";
+        String P4 = "*      ";
+        String P5 = "*      ";
+
+        // Line 1
+        System.out.println(String.join("   ", O1, O1, P1, P1));
+
+        // Line 2
+        System.out.println(String.join("   ", O2, O2, P2, P2));
+
+        // Line 3
+        System.out.println(String.join("   ", O3, O3, P3, P3));
+
+        // Line 4
+        System.out.println(String.join("   ", O4, O4, P4, P4));
+
+        // Line 5
+        System.out.println(String.join("   ", O5, O5, P5, P5));
     }
 }
